1cecf9e5732382ab308f7b7241c80f7a265b862a
HectorMtz22 <hec.gre81@gmail.com>
2021-06-10 18:34:51 -0500

    One form to return variables

--- Main.lua
+++ Main.lua
@@ -1,5 +1,23 @@
+-- One option to require a module
 require("welcome")
+
+-- Another option to require a module
+local mymath = require("mymath")
+
+
 local player = welcome()
+
+local readValues = function () 
+    print("Introduce one value")
+    local first = io.read()
+    print("Introduce another value")
+    local second = io.read()
+    return first, second
+end
+
+a , b = readValues()
+print(a, b)
+
 
 
 
